93837d641d25ea6d2ff3738b018e085d59ad1abc
BambooPanda <123456789+odin@users.noreply.github.com>
2025-04-11 11:30:59 -0700

    finished styling and javascript.

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+
+document.addEventListener("DOMContentLoaded", function () {
+  //store all components on HTML in our JS
+  let clear = document.querySelector("#clear-btn");
+  let equal = document.querySelector(".clear");
+  let decimal = document.querySelector(".decimal");
+
+  let numbers = document.querySelector(".number");
+  let operators = document.querySelector(".operator");
+
+  let previousScreen = document.querySelector(".previous");
+  let currentScreen = document.querySelector(".current");
+
+  numbers.forEach((number) =>
+    number.addEventListener("click", function (e) {
+      handleNumber(e.target.textContent);
+      currentScreen.textContent = currentValue;
+    })
+  );
+  operators.forEach((op) =>
+    op.addEventListener("click", function (e) {
+      handleOperator(e.target.textContent);
+      previousScreen.textContent = previousValue + "" + operator;
+      currentScreen.textContent = currentValue;
+    })
+  );
+});
+
+function handleNumber(num) {
+  if (currentValue.length <= 5) {
+    currentValue += num;
+  }
+}
+function handleOperator(op) {
+  operator = op;
+  previousValue = currentValue;
+  currentValue = "";
+}
+
 const btns = Array.from(document.querySelectorAll(".container"));
 
 const add = function (a, b) {
@@ -21,9 +63,3 @@ const divide = function (n) {
 };
 
 function operate() {}
-
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    bgColor = btn.value;
-  });
-});
