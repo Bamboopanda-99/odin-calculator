fc2a6c24e82edb2357ded7786d6edecfec2c6436
BambooPanda <123456789+odin@users.noreply.github.com>
2025-04-12 14:47:53 -0700

    finished the calculator

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ let previousValue = "";
 let currentValue = "";
 
 document.addEventListener("DOMContentLoaded", function () {
-  //store all components on HTML in our JS
   let clear = document.querySelector("#clear-btn");
-  let equal = document.querySelector(".clear");
+  let equal = document.querySelector(".equal");
   let decimal = document.querySelector(".decimal");
 
-  let numbers = document.querySelector(".number");
-  let operators = document.querySelector(".operator");
+  let numbers = document.querySelectorAll(".number");
+  let operators = document.querySelectorAll(".operator");
 
   let previousScreen = document.querySelector(".previous");
   let currentScreen = document.querySelector(".current");
@@ -27,6 +26,28 @@ document.addEventListener("DOMContentLoaded", function () {
       currentScreen.textContent = currentValue;
     })
   );
+  clear.addEventListener("click", function () {
+    previousValue = "";
+    currentValue = "";
+    operator = "";
+    previousScreen.textContent = currentValue;
+    currentScreen.textContent = currentValue;
+  });
+  equal.addEventListener("click", function () {
+    if (currentValue != "" && previousValue != "") {
+      calculate();
+      previousScreen.textContent = "";
+      if (previousValue.length <= 5) {
+        currentScreen.textContent = previousValue;
+      } else {
+        currentScreen.textContent = previousValue.slice(0, 5) + "...";
+      }
+    }
+  });
+
+  decimal.addEventListener("click", function () {
+    addDecimal();
+  });
 });
 
 function handleNumber(num) {
@@ -39,27 +60,30 @@ function handleOperator(op) {
   previousValue = currentValue;
   currentValue = "";
 }
+function calculate() {
+  previousValue = Number(previousValue);
+  currentValue = Number(currentValue);
 
-const btns = Array.from(document.querySelectorAll(".container"));
-
-const add = function (a, b) {
-  return a + b;
-};
-
-const sub = function (a, b) {
-  return a - b;
-};
-
-const multiply = function (array) {
-  return array.reduce((product, current) => product * current);
-};
-
-const divide = function (n) {
-  let product = 1;
-  for (i = n; i > 0; i--) {
-    product *= 1;
+  if (operator === "+") {
+    previousValue += currentValue;
+  } else if (operator === "-") {
+    previousValue -= currentValue;
+  } else if (operator === "x") {
+    previousValue *= currentValue;
+  } else if (operator === "/") {
+    previousValue /= currentValue;
   }
-  return product;
-};
 
-function operate() {}
+  previousValue = roundNumber(previousValue);
+  previousValue = previousValue.toString();
+  currentValue = previousValue.toString();
+}
+function roundNumber(num) {
+  return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal() {
+  if (!currentValue.includes(".")) {
+    currentValue += ".";
+  }
+}
